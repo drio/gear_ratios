83255eeae8096770e5cd871d6ae2887ccab0d9c4
David Rio Deiros <driodeiros@gmail.com>
2019-02-15 14:57:40 -0500

    nc

--- bin/to-apu.sh
+++ bin/to-apu.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
 
-ssh apu "rm -rf /usr/local/www/drio.org/public/cse512/week2/*"
-scp -r dist/* apu:/usr/local/www/drio.org/public/cse512/week2/
+dir_name=`basename $(pwd)`
+echo "We'll build and transfer the app to: publis/viz/${dir_name}"
+echo "Is that ok? <ENTER> to continue ..."
+read FOO
+
+echo "Building ..."
+./bin/build.sh
+
+echo "Transferring to apu ..."
+echo "dir: $dir_name"
+ssh apu mkdir -p /usr/local/www/drio.org/public/vizs/$dir_name
+scp -r dist/* apu:/usr/local/www/drio.org/public/vizs/$dir_name
+
+echo "🙌 http://drio.org/vizs/$dir_name"
